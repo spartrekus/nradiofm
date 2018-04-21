499e318afba5da51516b48fc08486c12a1e74742
spartrekus <spartrekus@gmail.com>
2018-04-21 20:47:10 +0200

    Add files via upload

---
+++ radiobleu.sh
@@ -0,0 +1,7 @@
+
+ mplayer  -vo null -ao alsa:device=hw=1,0 "http://direct.francebleu.fr/live/fb1071-midfi.mp3?ID=f9fbk29m84" 
+
+         mpg123 -a hw:1,0 "http://direct.francebleu.fr/live/fb1071-midfi.mp3?ID=f9fbk29m84" 
+
+exit
+
